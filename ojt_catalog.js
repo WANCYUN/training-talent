81ee2b62159d7e20cb06b760be4cc75d9bebd320
萬群 <zxc29030797@gmail.com>
2026-08-27 10:16:19 +0800

    Add OJT course template with details and manuals

---
+++ ojt_catalog.js
@@ -0,0 +1,23 @@
+window.OJT_COURSE_TEMPLATE = Object.freeze({
+  title: 'OJT課程',
+  description: '代理店副理 OJT 門市基礎必修與現場管理手冊',
+  completionRule: '所有必修子課程完成',
+  manuals: Object.freeze([
+    Object.freeze({
+      key: 'jjk-ojt-store-basic-2025-08-05',
+      lessonTitle: 'JJK｜OJT 門市基礎必修課（序）',
+      materialTitle: 'JJK-序、OJT 門市基礎必修課手冊',
+      revision: '2025-08-05',
+      pages: 7,
+      sort: 1,
+      summary: Object.freeze([
+        '稱職店副理應有的素養與 OJT 學習態度',
+        '門市現場環境認識：地址、電話、座位與逃生圖',
+        '店副理早班／晚班工作流程整理',
+        '門市商圈、友店、銀行、里辦公室與轄區資訊認識',
+        '財務管理：零找金、偽鈔辨識與異常結帳處理',
+        '人員認識、正向回饋與門市職能培育規劃'
+      ])
+    })
+  ])
+});
